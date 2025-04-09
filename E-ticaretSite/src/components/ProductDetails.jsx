3b441b81ecd5250557582390559c5b7e4b599dbd
egemencakir39 <115990783+egemencakir39@users.noreply.github.com>
2025-04-09 22:26:52 +0300

    Update ProductDetails.jsx

--- E-ticaretSite/src/components/ProductDetails.jsx
+++ E-ticaretSite/src/components/ProductDetails.jsx
@@ -61,13 +61,13 @@ function ProductDetails() {
                     <div style={{ display: 'flex' }}>
                         <ReactStars
                             count={5}
-                            value={rating.rate}
+                            value={rating?.rate}
                             size={24}
                             color2={'#ffd700'}
                             color1={'#ddd'}
                             edit={false}
                         />
-                        <p style={{ marginTop: "6px", marginLeft: "9px" }}> {rating.rate}  {rating.count} Değerlendirme</p>
+                        <p style={{ marginTop: "6px", marginLeft: "9px" }}> {rating?.rate}  {rating?.count} Değerlendirme</p>
 
                     </div>
                     <p>{description}</p>
@@ -77,9 +77,9 @@ function ProductDetails() {
                     <button onClick={addBasket} >Sepete ekle</button>
                 </div>
                 <div className='flex-row'>
-                    <button onClick={countPlus} >+</button>
+                    <button onClick={countPlus} style={{ padding: "10px", margin: "15px", fontSize: "18px", borderRadius: "5px", border: "1px solid black" }} >+</button>
                     <p>{count}</p>
-                    <button onClick={countDec} >-</button>
+                    <button onClick={countDec} style={{ padding: "10px", margin: "15px", fontSize: "18px", borderRadius: "5px", border: "1px solid black" }} >-</button>
                 </div>
             </div>
         </div>
